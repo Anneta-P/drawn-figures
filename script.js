bfb184ca370238f83fe8666b729c518600b4f067
Anneta <anneta@Users-MacBook-Pro.local>
2023-04-05 15:54:27 +0300

    right star pattern

--- script.js
+++ script.js
@@ -48,22 +48,39 @@
 // console.log(line);
 
 /////////////////СТРІЛКА ВЛІВО///////////////////
+// const len = 7;
+// let line = '';
+// for (let i = 1; i <= len; i++) {
+//   for (let j = 0; j < len - i; j++) {
+//     line += ' ';
+//   }
+//   for (let k = 0; k < i; k++) {
+//     line += '*';
+//   }
+//   line += '\n';
+// }
+// for (let i = 1; i <= len - 1; i++) {
+//   for (let j = 0; j < i; j++) {
+//     line += ' ';
+//   }
+//   for (let k = 0; k < len - i; k++) {
+//     line += '*';
+//   }
+//   line += '\n';
+// }
+// console.log(line);
+
+/////////////////СТРІЛКА ВПРАВО///////////////////
 const len = 7;
 let line = '';
 for (let i = 1; i <= len; i++) {
-  for (let j = 0; j < len - i; j++) {
-    line += ' ';
-  }
-  for (let k = 0; k < i; k++) {
+  for (let j = 0; j < i; j++) {
     line += '*';
   }
   line += '\n';
 }
 for (let i = 1; i <= len - 1; i++) {
-  for (let j = 0; j < i; j++) {
-    line += ' ';
-  }
-  for (let k = 0; k < len - i; k++) {
+  for (let j = 0; j < len - i; j++) {
     line += '*';
   }
   line += '\n';
