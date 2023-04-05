0c82e1242ad5574acbea3062442e0166585e439e
Anneta <anneta@Users-MacBook-Pro.local>
2023-04-05 14:52:10 +0300

    up arrow

--- script.js
+++ script.js
@@ -32,3 +32,17 @@
 // console.log(line);
 
 /////////////////СТРІЛКА ВГОРУ/////////
+let len = 7;
+let line = '';
+
+for (let i = 1; i <= len; i++) {
+  for (let j = 1; j <= len - i; j++) {
+    line += ' ';
+  }
+
+  for (let g = 0; g < 2 * i - 7; g++) {
+    line += '*';
+  }
+  line += '\n';
+}
+console.log(line);
