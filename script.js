70f5d3cce58d5591892df1d3ec8f1816d4be4744
AkaBussola <akabussola@Users-MacBook-Pro-2.local>
2023-06-22 15:28:03 +0300

    bug fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-///////////////   КВАДРАТ   +++++  //////////////
+///////////////   КВАДРАТ   //////////////
 
 // const len = 7;
 // let line = '';
@@ -14,32 +14,25 @@
 
 
 
-/////////////////СТРІЛКА ВНИЗ  ------- /////////
+/////////////////СТРІЛКА ВНИЗ  /////////
 
+
+// let i = 0,
+//   j = 0;
 // const len = 7;
+// let space = "",
+//   star = "";
 
-// const heigh = Math.ceil(len / 2);
-// for (let heighIndex = 0; heighIndex < heigh; heighIndex++) {
-//   const starts = len - heighIndex * 2;
-//   console.log(' '.repeat(heighIndex) + '*'.repeat(starts));
+// for (i = 3; i >= 0; i--) {
+//   space = "";
+//   star = "";
+//   for (j = 0; j < len - i; j++) space += " ";
+//   for (j = 0; j < 2 * i + 1; j++) star += "*";
+//   console.log(space + star);
 // }
 
-let i = 0,
-  j = 0;
-const len = 7;
-let space = "",
-  star = "";
 
-for (i = 3; i >= 0; i--) {
-  space = "";
-  star = "";
-  for (j = 0; j < len - i; j++) space += " ";
-  for (j = 0; j < 2 * i + 1; j++) star += "*";
-  console.log(space + star);
-}
-
-
-/////////////////СТРІЛКА ВГОРУ  ++++++/////////
+/////////////////  СТРІЛКА ВГОРУ /////////
 
 // let i = 0,
 //   j = 0;
@@ -59,39 +52,23 @@ for (i = 3; i >= 0; i--) {
 
 /////////////СТРІЛКА ВЛІВО --------- ///////////////
 
-// const len = 7;
-// const mid = Math.floor(len / 2);
-
-// for (let i = 1; i <= len; i++) {
-//   let spaces = '';
-//   let stars = '';
-
-//   if (i <= mid) {
-//     spaces = ' '.repeat(mid - i);
-//     stars = '*'.repeat(i);
-//   } else {
-//     spaces = ' '.repeat(i - mid - 1);
-//     stars = '*'.repeat(len - i + 1);
-//   }
-
-//   console.log(spaces + stars);
-// }
 
 
 
-/////////////////СТРІЛКА ВПРАВО  +++++++++ ///////////////////
-// const len = 7;
-// const mid = Math.floor(len / 2);
 
-// for (let i = 1; i <= len; i++) {
-//   let stars = '';
+/////////////////СТРІЛКА ВПРАВО ///////////////////
+const len = 7;
+const mid = Math.floor(len / 2);
 
-//   if (i <= mid) {
-//     stars = '*'.repeat(i);
-//   } else {
-//     stars = '*'.repeat(len - i + 1);
-//   }
+for (let i = 1; i <= len; i++) {
+  let stars = '';
 
-//   console.log(stars);
-// }
+  if (i <= mid) {
+    stars = '*'.repeat(i);
+  } else {
+    stars = '*'.repeat(len - i + 1);
+  }
+
+  console.log(stars);
+}
 
